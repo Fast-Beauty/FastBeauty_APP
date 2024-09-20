ee5c189493612a6ccab61dbe55f8203e8b7b910d
Alejandro Betancourth <luisbetancourth43@gmail.com>
2024-09-19 22:28:39 -0500

    Se añade api firebase

--- view/appointmentsScreen.tsx
+++ view/appointmentsScreen.tsx
@@ -4,34 +4,62 @@ import { Calendar } from 'react-native-calendars';
 import Carousel from 'react-native-reanimated-carousel';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import {service} from '../api/service.js';
+import { service } from '../api/service.js';
 
 export default function Appointments() {
+
+    const [servicios, setServices] = useState([]);
+    const [employees, setEmployees] = useState([]);
+
+    const fetchService = async () => {
+        try {
+            const response = await fetch('https://apiappfastbeauty-default-rtdb.firebaseio.com/services.json');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            setServices(data);
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
+    const fetchEmployees = async () => {
+        try {
+            const response = await fetch('https://apiappfastbeauty-default-rtdb.firebaseio.com/employees.json');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            setEmployees(data);
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
+
+    console.log(servicios)
+
     const width = Dimensions.get('window').width;
-    const data = [
-        { url: 'https://cdn.pixabay.com/photo/2024/05/06/14/23/ai-generated-8743405_640.png', id: 1 },
-        { url: 'https://cdn.pixabay.com/photo/2023/03/14/05/01/ai-generated-7851469_1280.jpg', id: 2 },
-        { url: 'https://cdn.pixabay.com/photo/2023/01/19/15/32/squirrel-7729415_1280.jpg', id: 3 },
-        { url: 'https://cdn.pixabay.com/photo/2022/12/12/12/58/dog-7651002_1280.jpg', id: 4 }
-    ];
-    const [services, setServices] = useState([]);
+    // const servicios = [
+    //     { url: require('../assets/images/services/service-cejas.jpg'), id: 1, name: 'Cejas' },
+    //     { url: require('../assets/images/services/service-corte.jpg'), id: 2, name: 'Corte' },
+    //     { url: require('../assets/images/services/service-depilacion.jpg'), id: 3, name: 'Depilación' },
+    //     { url: require('../assets/images/services/service-manicure.jpg'), id: 4, name: 'Manicure' },
+    //     { url: require('../assets/images/services/service-maquillaje.jpg'), id: 5, name: 'Maquillaje' },
+    //     { url: require('../assets/images/services/service-masaje.jpg'), id: 6, name: 'Masaje' },
+    //     { url: require('../assets/images/services/service-tinturado.jpg'), id: 7, name: 'Tinturado' },
+    // ];
+    // const employees = [
+    //     { url: require('../assets/images/employees/estilista-1.jpg'), id: 1, name: 'Estilista 1' },
+    //     { url: require('../assets/images/employees/estilista-2.jpg'), id: 2, name: 'Estilista 2' },
+    //     { url: require('../assets/images/employees/estilista-3.jpg'), id: 3, name: 'Estilista 3' },
+    //     { url: require('../assets/images/employees/estilista-4.jpg'), id: 4, name: 'Estilista 4' },
+    //     { url: require('../assets/images/employees/estilista-5.jpg'), id: 5, name: 'Estilista 5' },
+    // ];
+
     const insets = useSafeAreaInsets();
     const [selectedDate, setSelectedDate] = useState('');
     const [selectedService, setSelectedService] = useState('');
     const [selectedEmployee, setSelectedEmployee] = useState('');
-
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const data = await service();  // Llamar a la API
-            setServices(data);  // Guardar los datos en el estado
-          } catch (error) {
-            console.error(error);
-          }
-        };
-    
-        fetchData();  // Llamar a la función fetchData cuando el componente se monte
-      }, []);
 
     const cita = {
         status: "Espera",
@@ -45,11 +73,32 @@ export default function Appointments() {
     const handleService = (index) => {
         setSelectedService(index.id);
         console.log(index.id);
-    } 
+    }
     const handleEmployee = (index) => {
         setSelectedEmployee(index.id);
         console.log(index.id);
-    } 
+    }
+
+    const handleSend = async (cita) => {
+        try {
+            await fetch('http://localhost:8080/appointments/create', {
+                method: 'POST',
+                body: JSON.stringify(cita),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            window.location.href = 'index.html';
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchService();
+        fetchEmployees();
+    }, []);
+
 
     return (
         <View style={[styles.container, { paddingTop: insets.top }]}>
@@ -64,15 +113,16 @@ export default function Appointments() {
                         height={width / 2}
                         autoPlay={false}
                         autoPlayInterval={3000}
-                        data={data}
+                        data={servicios}
+                        mode='parallax'
                         scrollAnimationDuration={1000}
                         onSnapToItem={(index) => console.log('current index:', index)}
                         renderItem={({ index }) => (
-                            <TouchableOpacity style={{flex: 1}} onPress={() => handleService(data[index])}>
-                                <Text>{data[index].id}</Text>
+                            <TouchableOpacity style={{ flex: 1 }} onPress={() => handleService(servicios[index])}>
+                                <Text style={{ fontSize: 22, textAlign: 'center' }}>{servicios[index].name}</Text>
                                 <View style={[styles.carouselItem]}>
                                     <Image
-                                        source={{ uri: data[index].url }}
+                                        source={servicios[index].url}
                                         style={styles.image}
                                         resizeMode="cover"
                                     />
@@ -87,18 +137,19 @@ export default function Appointments() {
                     <Carousel
                         loop={true}
                         width={width}
-                        height={width / 2}
+                        height={width * 1.5}
                         autoPlay={false}
                         autoPlayInterval={3000}
-                        data={data}
+                        data={employees}
+                        mode='parallax'
                         scrollAnimationDuration={1000}
                         onSnapToItem={(index) => console.log('current index:', index)}
                         renderItem={({ index }) => (
-                            <TouchableOpacity style={{flex: 1}} onPress={() => handleEmployee(data[index])}>
-                                <Text>{data[index].id}</Text>
+                            <TouchableOpacity style={{ flex: 1 }} onPress={() => handleEmployee(employees[index])}>
+                                <Text style={{ fontSize: 22, textAlign: 'center' }}>{employees[index].name}</Text>
                                 <View style={styles.carouselItem}>
                                     <Image
-                                        source={{ uri: data[index].url }}
+                                        source={employees[index].url}
                                         style={styles.image}
                                         resizeMode="cover"
                                     />
@@ -122,29 +173,18 @@ export default function Appointments() {
                         <Text style={styles.statusText}>No disponible</Text>
                     </View>
                 </View>
-                {/* <Calendar
-                    onDayPress={date => {
-                        console.log('selected day', date);
-                    }}
-                    markedDates={{
-                        [selected]: { selected: true, disableTouchEvent: true, selectedDotColor: 'orange' }
-                    }}
-                /> */}
 
                 <View style={{ alignItems: 'center' }}>
                     <Calendar
-                        onDayPress={(day) => setSelectedDate(day.dateString)} // Se activa cuando se presiona una fecha
+                        onDayPress={(day) => setSelectedDate(day.dateString)}
                         markedDates={{
-                            [selectedDate]: { selected: true, selectedColor: '#2B2235' }, // Marca la fecha seleccionada
+                            [selectedDate]: { selected: true, selectedColor: '#2B2235' },
                         }}
                     />
-                    <Text>Fecha seleccionada: {selectedDate}</Text>
                 </View>
 
                 <View style={{ alignItems: 'center', marginVertical: 30 }}>
-                    <TouchableOpacity style={styles.button} onPress={() => {
-                        console.log(cita);
-                    }}>
+                    <TouchableOpacity style={styles.button} onPress={() => handleSend(cita)}>
                         <Text style={styles.buttonText}>AGENDAR</Text>
                     </TouchableOpacity>
                 </View>
@@ -174,7 +214,7 @@ const styles = StyleSheet.create({
         fontSize: 24,
         fontWeight: 'semibold',
         textAlign: 'center',
-        marginVertical: 20,
+        marginTop: 20,
         color: '#2B2235',
     },
     title: {
