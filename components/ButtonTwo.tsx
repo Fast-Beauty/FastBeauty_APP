c6a4e4ade1ab38892ddd24f539b5714e350f0e6c
fabianCifu <fafecic@gmail.com>
2024-09-19 21:22:56 -0500

    se agrega diseno vista login register

--- components/ButtonTwo.tsx
+++ components/ButtonTwo.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 
-const ButtonTwo = ({ title, onPress, color = '#ffc0cb' }) => {
+const ButtonTwo = ({ title, onPress, color = '#050A24' }) => {
   return (
     <TouchableOpacity style={[styles.button, { backgroundColor: color }]} onPress={onPress}>
       <Text style={styles.buttonText}>{title}</Text>
@@ -11,10 +11,15 @@ const ButtonTwo = ({ title, onPress, color = '#ffc0cb' }) => {
 
 const styles = StyleSheet.create({
   button: {
-    padding: 15,
+    padding: 5,
     borderRadius: 10,
     alignItems: 'center',
-    marginVertical: 10,
+    marginRight: 80,
+    marginLeft: 80,
+    marginTop: 10,
+    marginBottom:20,
+    borderWidth: 2,
+    borderColor: '#ffc0cb',
   },
   buttonText: {
     color: '#fff',
